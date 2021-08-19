1c70b473127790cab236fa8138d3676e4588799a
Lakshaya Utreja <lakshaya@hackatunes.cf>
2021-08-19 13:03:38 +0530

    add typdef for manageStudentController

---
+++ controllers/manageStudentsController.d.ts
@@ -0,0 +1,5 @@
+export function manageStudentsController(req: express.Request, res: express.Response, next: express.NextFunction): void;
+export function deleteStudentController(req: express.Request, res: express.Response, next: express.NextFunction): void;
+export function editStudentController(req: express.Request, res: express.Response, next: express.NextFunction): void;
+export function newStudentController(req: express.Request, res: express.Response, next: express.NextFunction): void;
+import express = require('express');
