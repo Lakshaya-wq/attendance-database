30f39edeba63bdddf74b014b0303b07bdc65d0e3
Lakshaya Utreja <lakshaya@hackatunes.cf>
2021-12-23 11:10:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
+const { env } = process;
+
 exports.DB_URI =
     process.env.NODE_ENV === "production"
-        ? process.env.DB_URI
-        : "mongodb://localhost:27017/attendanceDatabase";
-exports.PORT = process.env.PORT || 3000;
-exports.SESSION_SECRET = process.env.SESSION_SECRET;
+        ? `mongodb+srv://${env.DB_USER}:${env.DB_PASSWORD}@${env.DB_HOST}/${env.DB_NAME}?retryWrites=true&w=majority`
+        : "mongodb://localhost:27017/attendance-database";
+exports.PORT = env.PORT || 3000;
+exports.SESSION_SECRET = env.SESSION_SECRET;
